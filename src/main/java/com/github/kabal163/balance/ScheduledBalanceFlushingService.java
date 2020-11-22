4339ae2793c7bcfb86448fc6074c19421dbd49c0
Dmitrii <cs159753@yandex.ru>
2020-11-22 17:04:47 +0300

    refactoring

--- src/main/java/com/github/kabal163/balance/ScheduledBalanceFlushingService.java
+++ src/main/java/com/github/kabal163/balance/ScheduledBalanceFlushingService.java
@@ -8,8 +8,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 @Service
@@ -24,7 +22,9 @@ public class ScheduledBalanceFlushingService implements BalanceFlushingService {
     public void flush() {
         log.trace("Flushing wallet's balance...");
 
-        Wallet wallet = walletRepository.findById(UUID.fromString("b1116a97-1e7c-484b-a111-89cc718c7772")).orElseThrow();
+        Wallet wallet = walletRepository
+                .findById(UUID.fromString("b1116a97-1e7c-484b-a111-89cc718c7772"))
+                .orElseThrow();
         wallet.deposit(localBalanceManager.countAndReset());
         walletRepository.save(wallet);
 
