ff7b54cc586da4ce555b6e03392e61a87472fb17
Dmitrii <cs159753@yandex.ru>
2020-11-22 22:40:09 +0300

    fixed java doc

--- src/main/java/com/github/kabal163/wallet/WalletCommandService.java
+++ src/main/java/com/github/kabal163/wallet/WalletCommandService.java
@@ -1,5 +1,6 @@
 package com.github.kabal163.wallet;
 
+import com.github.kabal163.balance.BalanceFlushingService;
 import com.github.kabal163.transfer.ImmutableTransfer;
 
 /**
@@ -11,8 +12,11 @@ public interface WalletCommandService {
     /**
      * Deposits money to a wallet according the {@code command} and
      * returns an immutable copy of created transfer
+     * It's fast enough and can serve multiple deposits simultaneously.
+     * Result balance will not be stored in database immediatly
+     * but when {@link BalanceFlushingService#flush()} is completed.
      *
-     * @param command contains information about depositing transfer Must not be {@code null}
+     * @param command contains information about depositing transfer. Must not be {@code null}
      * @return immutable copy of newly created transfer
      * @throws IllegalArgumentException if {@code command} argument is {@code null}
      */
