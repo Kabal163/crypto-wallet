df503efd3c53006acbcb016d59afcc7867834899
Dmitrii <cs159753@yandex.ru>
2020-11-22 18:24:08 +0300

    Made it more convenient

--- src/main/java/com/github/kabal163/balance/LocalBalance.java
+++ src/main/java/com/github/kabal163/balance/LocalBalance.java
@@ -2,12 +2,27 @@ package com.github.kabal163.balance;
 
 import java.math.BigDecimal;
 
+/**
+ * Thread safe.
+ * Represents a per thread balance.
+ */
 public class LocalBalance {
 
     private final Object lock = new Object();
-    private BigDecimal amount = new BigDecimal(0);
+    private BigDecimal amount;
+
+    public LocalBalance() {
+        this(new BigDecimal(0));
+    }
+
+    public LocalBalance(BigDecimal initAmount) {
+        this.amount = initAmount;
+    }
 
     public BigDecimal deposit(BigDecimal value) {
+        if (value.compareTo(new BigDecimal(0)) < 0) {
+            throw new IllegalArgumentException("Deposited value must not be negative! actual: " + value);
+        }
         synchronized (lock) {
             amount = amount.add(value);
             return amount;
